3b5a781f0b2da9f485a7b2f4e7ba00c5c456252d
MadsHaderup <mads@haderup.net>
2021-11-29 11:52:56 +0100

    Started implementing subproject - not working

--- src/main/java/examProject2/web/ProjectController.java
+++ src/main/java/examProject2/web/ProjectController.java
@@ -26,6 +26,7 @@ public class ProjectController {
         } else {
             model.addAttribute("projects", projectService.fetchProjects(user));
         }
+
         return "mainPage";
     }
 
@@ -36,10 +37,22 @@ public class ProjectController {
     public RedirectView createProject(WebRequest request) throws ExamProjectException {
         String projectname = request.getParameter("projectName");
         User user = (User) request.getAttribute("user", WebRequest.SCOPE_SESSION);
-        System.out.println(user.getUserID() + " " + user.getUsername());
         assert user != null;
         Project project = projectService.createProject(projectname, user.getUserID());
         request.setAttribute("project", project, WebRequest.SCOPE_SESSION);
         return new RedirectView("mainPage");
+    }
+
+    @GetMapping("/deleteProject")
+    public RedirectView deleteProject(int projectID) {
+        projectService.deleteProject(projectID);
+        return new RedirectView("mainPage");
+    }
+
+    @GetMapping("/projectsPage")
+    public RedirectView projectsPage(int projectID, Model model){
+
+        model.addAttribute("subprojects", projectService.fetchSubprojects(projectID));
+        return new RedirectView("projectsPage");
     }
 }
